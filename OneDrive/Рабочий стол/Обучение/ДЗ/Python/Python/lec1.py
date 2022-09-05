ac1576605d6e109aa4ebc0db1bc0cac3437f0eb7
Anastasia <vorobyova.86@mail.ru>
2022-09-05 20:10:02 +0300

    seminar 1 task 1 will be done soon

---
+++ OneDrive/Рабочий стол/Обучение/ДЗ/Python/Python/lec1.py
@@ -0,0 +1,55 @@
+#print('hello world')
+
+#value = None
+#print(type(value))
+#a = 123
+#b = 1.23
+#print(type(a))
+#print(type(b))
+#value = 1234
+#print(type(value))
+
+#s = 'hello \nworld'
+#print(s)
+#print(a,' - ', b, '-', s)
+#print('{} - {} - {}'.format(a,b,s))
+#print(f'{a} - {b} - {s}')
+#print('{1} - {2} - {0}'.format(a,b,s))
+
+#f = False
+#print(f)
+
+#list = ['1','2','3']
+#col = ['hello', 1,2,3, 4.5, True]
+#print(list)
+#print(col)
+
+#print('Введите a');
+#a = float(input())
+#print('Введите b');
+#b = float(input())
+#print(a, ' + ', b, ' = ', a+b)
+#print('{} {}'.format(a, b))
+#print(f'{a} {b}')
+
+#a = 1.31231223
+#b = 3
+#c = round(a * b,7)
+#print(c)
+
+#a = 3
+#a += 5
+#print(a)
+
+#f = [1 , 2, 3, 4]
+#print(f)
+#print(not 2 in f)
+#is_odd = not f[0] % 2 
+#print(is_odd)
+
+a = int(input('a = '))
+b = int(input('b = '))
+if a > b:
+    print(a)
+else:
+    print(b)    
---
+++ OneDrive/Рабочий стол/Обучение/ДЗ/Python/Python/sem1.py/sem1.py
@@ -0,0 +1,15 @@
+
+#list = [1 , 4 , 6 , 2 , 5]
+#max_number = 0
+#for i in range(len(list)) :
+#    if list[i] > max_number :
+#        max_number =  list[i]
+#print(max_number)
+
+
+#number = int (input("Введите число : "))
+#i = number * -1
+#while i < number + 1 :
+#    print(i)
+#    i += 1
+ 
