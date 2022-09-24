5584534b177ac746ce7cfad50d543eaddcc3aaed
Anastasia <vorobyova.86@mail.ru>
2022-09-24 22:27:19 +0300

    task3 is done

--- OneDrive/Рабочий стол/Обучение/ДЗ/Python/Python/Sem2.py/sem2_task3.py
+++ OneDrive/Рабочий стол/Обучение/ДЗ/Python/Python/Sem2.py/sem2_task3.py
@@ -1,3 +1,8 @@
 #Задайте список из k чисел последовательности (1 + 1\k)^k и выведите на экран их сумму.
 
-num = int (input('Введите число: '))
+num = int (input('Введите число: ')) 
+def receive_sequence(num):
+    return[round((1 + 1 / k)**k, 3) for k in range (1, num + 1)]
+
+print(receive_sequence(num))
+print(round(sum(receive_sequence(num))))
