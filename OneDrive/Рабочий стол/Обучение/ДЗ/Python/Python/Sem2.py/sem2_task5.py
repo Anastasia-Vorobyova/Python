355bf44883a019a2f4542be64eeba110e0846702
Anastasia <vorobyova.86@mail.ru>
2022-09-25 00:01:52 +0300

    task5 is done

--- OneDrive/Рабочий стол/Обучение/ДЗ/Python/Python/Sem2.py/sem2_task5.py
+++ OneDrive/Рабочий стол/Обучение/ДЗ/Python/Python/Sem2.py/sem2_task5.py
@@ -1,1 +1,11 @@
 #Реализуйте алгоритм перемешивания списка.
+
+import random
+list_1 = [1, 2, 3, 4, 5]
+print ("1й список : " + str(list_1))
+for i in range(len(list_1) -1, 0, -1):
+    j = random.randint(0, i + 1) 
+    list_1[i], list_1[j] = list_1[j], list_1[i] 
+
+print ("Перемешанный список: " +  str(list_1))
+
