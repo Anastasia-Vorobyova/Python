23648766d9d558d609a3107ec4a19d050aa4fa20
Anastasia <vorobyova.86@mail.ru>
2022-09-24 23:33:03 +0300

    task4 is done

--- OneDrive/Рабочий стол/Обучение/ДЗ/Python/Python/Sem2.py/sem2_task4.py
+++ OneDrive/Рабочий стол/Обучение/ДЗ/Python/Python/Sem2.py/sem2_task4.py
@@ -1,2 +1,15 @@
 #Задайте список из N элементов, заполненных числами из промежутка [-N, N].
 #Найдите произведение элементов на указанных пользователем через пробел позициях.
+
+from random import randint
+list = int(input('Введите число элемента: '))
+numbers = []
+for i in range(list):
+    numbers.append(randint(-list,list))
+print(numbers)
+
+a = int(input('Введите позицию первого элемента: '))
+b = int(input('Введите позицию второго элемента: '))
+for i in range(len(numbers)):
+    multiplication = numbers[a -1] * numbers[b - 1]
+print(f'Произведение элементов: {numbers[a -1]} * {numbers[b -1]} =', multiplication)
