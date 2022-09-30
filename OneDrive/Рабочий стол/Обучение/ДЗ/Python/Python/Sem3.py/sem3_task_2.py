98ef8cbeb33e73edff988346f238b502ee0401b0
Anastasia <vorobyova.86@mail.ru>
2022-10-01 00:03:26 +0300

    task2 is done

--- OneDrive/Рабочий стол/Обучение/ДЗ/Python/Python/Sem3.py/sem3_task_2.py
+++ OneDrive/Рабочий стол/Обучение/ДЗ/Python/Python/Sem3.py/sem3_task_2.py
@@ -1,17 +1,14 @@
 #2. Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
-# # Пример:
+#  Пример:
 
-# # - [2, 3, 4, 5, 6] => [12, 15, 16];
-# # - [2, 3, 5, 6] => [12, 15]
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+#  - [2, 3, 5, 6] => [12, 15]
 
-def mult_lst(lst):
-    l = len(lst)//2 + 1 
-    if len(lst) % 2 != 0 
-    else len(lst)//2
-    new_lst = [lst[i]*lst[len(lst)-i-1] 
-    for i in range(l)]
-    print(new_lst)
+def common_list(my_list):
+    list = len(my_list)//2 + 1 if len(my_list) % 2 != 0 else len(my_list)//2
+    new_list = [my_list[i]*my_list[len(my_list)-i-1] for i in range(list)]
+    print(new_list)
 
-lst = [2, 3, 4, 5, 6]
-mult_lst(lst)
+my_list = [2, 3, 4, 5, 6, 7, 8]
+common_list(my_list)
